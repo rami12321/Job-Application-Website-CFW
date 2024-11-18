62e728f81c270b7d9e47ed07c851ce3b8df03e21
Basharalsayyed2000 <bashar.alsayyed.20@gmail.com>
2024-11-18 10:15:22 +0200

    new Component

---
+++ src/app/Employer/SignUp-Employer/SignUp-Employer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-SignUp-Employer',
+  templateUrl: './SignUp-Employer.component.html',
+  styleUrls: ['./SignUp-Employer.component.css']
+})
+export class SignUpEmployerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Youth/SignUp-Youth/SignUp-Youth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-SignUp-Youth',
+  templateUrl: './SignUp-Youth.component.html',
+  styleUrls: ['./SignUp-Youth.component.css']
+})
+export class SignUpYouthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
