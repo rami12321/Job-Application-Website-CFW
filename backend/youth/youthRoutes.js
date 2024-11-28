2c49d4fa39d790d1f59f2d9994b4dcdabc073ffa
rami12321 <ramiabueid828@gamail.com>
2024-11-28 10:46:37 +0200

    without backend

--- backend/youth/youthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-
-router.get('/', (req, res) => {
-    res.send('This is the youth routes endpoint');
-});
-module.exports = router;
