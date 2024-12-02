8a1516f5e8f00af1863abd251c831ad02a746bd3
Basharalsayyed2000 <bashar.alsayyed.20@gmail.com>
2024-12-02 13:21:15 +0200

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Common/Login/Login.component';
 import { SignUpYouthComponent } from './Youth/SignUp-Youth/SignUp-Youth.component';
-import { MainEmployerComponent } from './Employer/main-employer/main-employer.component';
 import { MainAdminComponent } from './Admin/Main-Admin/Main-Admin.component';
+import { YouthSignupDetailsComponent } from './Youth/Details-Youth/Detailsyouth.component';
+import { MainEmployerComponent } from './Employer/Main-Employer/Main-Employer.component';
 import { SignUpEmployerComponent } from './Employer/SignUp-Employer/SignUp-Employer.component';
-import { YouthSignupDetailsComponent } from './Youth/SignUp-Youth-Details/youthsignup-details/youthsignup-details.component';
+import { MainYouthComponent } from './Youth/Main-Youth/Main-Youth.component';
 
 export const routes: Routes = [
   {
@@ -26,18 +27,21 @@ export const routes: Routes = [
   },
   {
     path: 'main-youth',
-    component: SignUpYouthComponent,
+    component: MainYouthComponent,
   },
   {
     path: 'signup-youth',
     component: SignUpYouthComponent, // Parent for steps
   },
-  
+
   {
-    path: 'signup-employer',
+    path: 'SignUp-Employer',
     component: SignUpEmployerComponent, // Parent for steps
   },
-  { path: 'youthsignup-details', component: YouthSignupDetailsComponent }, // Add this route
+  {
+    path: 'details-youth',
+    component: YouthSignupDetailsComponent
+  }, // Add this route
 
 
   {
