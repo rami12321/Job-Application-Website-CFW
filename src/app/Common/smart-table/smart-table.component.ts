f5c4f840e7e20fafb9761181f79cc174b2239a13
rami12321 <ramiabueid828@gamail.com>
2024-12-07 01:19:33 +0200

    anaaa

--- src/app/Common/smart-table/smart-table.component.ts
+++ src/app/Common/smart-table/smart-table.component.ts
@@ -13,6 +13,7 @@ import { ButtonModule } from 'primeng/button';
 import { LookupService } from '../../Services/LookUpService/lookup.service';
 import { YouthSignupDetailsComponent } from '../../Youth/Details-Youth/Detailsyouth.component';
 import { EmployerService } from '../../Services/employer-service/employer-services.service';
+import { DetailsEmployerComponent } from "../../Employer/Details-Employer/details-employer.component";
 interface Column {
   field: string;
   header: string;
@@ -31,7 +32,8 @@ interface Column {
     DialogModule,
     ButtonModule,
     YouthSignupDetailsComponent,
-  ],
+    DetailsEmployerComponent
+],
   providers: [YouthServiceService],
   templateUrl: './smart-table.component.html',
   styleUrl: './smart-table.component.css',
