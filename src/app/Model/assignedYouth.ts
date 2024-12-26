3c6294ff13bb27da6a31649ff8472ff348e2c531
rami12321 <ramiabueid828@gamail.com>
2024-12-26 13:07:25 +0200

    haha

--- src/app/Model/assignedYouth.ts
+++ src/app/Model/assignedYouth.ts
@@ -1,12 +1,11 @@
 export interface AssignedYouth {
-    id: number;
-    firstName: string;
-    lastName: string;
-    dob: string; // Assuming it's a string or date format
-    cv: string; // Assuming the CV is a URL or path to the file
-    status: string;
-    action?: 'accepted' | 'rejected' | 'approved' | null; // Optional action property
-    isDisabled?: boolean; 
+  id: number;
+  firstName: string;
+  lastName: string;
+  dob: string;
+  cv: string;
+  status: string;
+  action?: 'accepted' | 'rejected' | 'approved' | null;
+  isDisabled?: boolean;
 
-  }
-  
+}
